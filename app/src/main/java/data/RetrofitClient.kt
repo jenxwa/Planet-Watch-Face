4e8b3e936b63723bcd7a4fb514839cd015028cd7
jennifer.warbeck <jenx.wa@gmail.com>
2025-03-29 14:33:44 +0100

    Add custom trust manager to RetrofitClient to fix HandshakeException

--- app/src/main/java/data/RetrofitClient.kt
+++ app/src/main/java/data/RetrofitClient.kt
@@ -2,8 +2,11 @@ package data
 
 import ApiService
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.KeyStore
+import javax.net.ssl.*
 
 object RetrofitClient {
     private const val BASE_URL = "https://ssd.jpl.nasa.gov/api/"
@@ -11,14 +14,33 @@ object RetrofitClient {
         .setLenient()
         .create()
 
+    private fun getUnsafeOkHttpClient(): OkHttpClient {
+        return try {
+            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {}
+                override fun checkServerTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {}
+                override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> = arrayOf()
+            })
+
+            val sslContext = SSLContext.getInstance("TLS")
+            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+            OkHttpClient.Builder()
+                .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+                .hostnameVerifier { _, _ -> true }
+                .build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create(lenientGson)) // Use the custom Gson instance
+        .client(getUnsafeOkHttpClient())
+        .addConverterFactory(GsonConverterFactory.create(lenientGson))
         .build()
-
 
     val apiService: ApiService by lazy {
         retrofit.create(ApiService::class.java)
     }
-
 }
